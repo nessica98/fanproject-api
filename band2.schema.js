56cb87b55917f710fe36b117f77129c815c85492
Jisung Hwang <poksin.w@gmail.com>
2020-12-23 12:10:05 +0700

    BAND fix bug

--- band2.schema.js
+++ band2.schema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema
 
-var artist_sche = new schema({bandName:String,debut_date:Date,bandProfilePicURL:String,members:[schema.Types.ObjectId]},{ collection: 'Band_A' })
+var artist_sche = new schema({bandName:String,debut_date:Date,bandProfilePicURL:String,members:[schema.Types.ObjectId]},{ collection: 'Band' })
 
 let Band = mongoose.model('Band',artist_sche)
 
